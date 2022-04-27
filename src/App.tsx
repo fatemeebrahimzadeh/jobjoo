03c20f563410f92977f88105de081ee8709bd194
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-04-27 23:55:55 +0430

    change N to n in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Router from './routes';
 import './App.css';
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Router />
     </div>
   );
