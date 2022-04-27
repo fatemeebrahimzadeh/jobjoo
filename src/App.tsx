c798e23e3f3bcec477f2282dfa17ab20f5f9ee45
Davood Feridoni <davood2fanferidoni@gmail.com>
2022-04-27 23:36:21 +0430

    add selectBox to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Router from './routes';
 import './App.css';
-import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <Navbar />
       <Router />
     </div>
   );
