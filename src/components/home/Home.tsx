eacf69e82afb2b267777d268338b0b579a910421
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-04-27 23:18:38 +0430

    fix bug of img

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import "./Home.scss"
+import vector from "../../assets/img/vector.png"
 
 export default class Home extends Component {
     render() {
         return (
             <main className="HomePage">
                 <div className="homeImg">
-                    <img src="./Image 2.png" alt="vector" />
+                    <img src={vector} alt="vector" />
                 </div>
                 <section className="searchSection">
                     <h1>جابجو بزرگترین سامانه کاریابی آنلاین</h1>
                     <h3>موتور جستجو هوشمند آگهی</h3>
                     <div className="selectBox"></div>
+                    {/* should be dynamic */}
                     <div className="companyLogos">
                         <div className="companyLogo"></div>
                         <div className="companyLogo"></div>
