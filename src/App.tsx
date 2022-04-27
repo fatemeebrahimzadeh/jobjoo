77acc0197d85ef686a94a202daa866778d1e96d6
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-04-27 17:29:33 +0430

    chaneg navbarname

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Router from './routes';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
