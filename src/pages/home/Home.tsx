fecf027705700075c877fb3341dc6d382ddf4f8a
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-04-30 22:07:14 +0430

    add company logos to HomePage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,31 +2,58 @@ import React, { Component } from "react";
 import "./Home.scss"
 import vector from "../../assets/img/vector.png"
 import SelectBox from "../../components/selectBox/SelectBox";
+import iranEstekhdamLogo from "../../assets/img/iranEstekhdamLogo.png"
+import jobinjaLogo from "../../assets/img/jobinjaLogo.png"
+import jobvisionLogo from "../../assets/img/jobvisionLogo.png"
+import sheypourLogo from "../../assets/img/sheypourLogo.png"
+
+let companyLogos = [
+    {
+        img: sheypourLogo,
+        linkAddress: "https://www.sheypoor.com/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85"
+    },
+    {
+        img: jobinjaLogo,
+        linkAddress: "https://jobinja.ir/"
+    },
+    {
+        img: jobvisionLogo,
+        linkAddress: "https://jobvision.ir/"
+    },
+    {
+        img: iranEstekhdamLogo,
+        linkAddress: "https://iranestekhdam.ir/"
+    },
+    {
+        img: sheypourLogo,
+        linkAddress: "https://www.sheypoor.com/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85"
+    },
+    {
+        img: jobinjaLogo,
+        linkAddress: "https://jobinja.ir/"
+    }
+]
 
 export default class Home extends Component {
     render() {
+
+        let companyLogosImage = companyLogos.map(companyLogo => {
+            return <a href={companyLogo.linkAddress}>
+                <img src={companyLogo.img} alt="companyLogo" />
+            </a>
+        })
+
         return (
             <main className="HomePage">
                 <div className="homeImg">
                     <img src={vector} alt="vector" />
                 </div>
                 <section className="searchSection">
-                    <h1>جابجو بزرگترین سامانه کاریابی آنلاین</h1>
-                    <h3>موتور جستجو هوشمند آگهی</h3>
-                    <div className="selectBox">
-                        <SelectBox />
-                    </div>
-                    {/* should be dynamic */}
+                    <h1>جابجو بزرگترین سامانه جست‌و‌جوی آگهی استخدام </h1>
+                    <h3>با بیش از 1000 آگهی بروز از سایت‌های معتبر کاریابی</h3>
+                    <SelectBox />
                     <div className="companyLogos">
-                        <div className="companyLogo"></div>
-                        <div className="companyLogo"></div>
-                        <div className="companyLogo"></div>
-                        <div className="companyLogo"></div>
-                        <div className="companyLogo"></div>
-                        <div className="companyLogo"></div>
-                        <div className="companyLogo"></div>
-                        <div className="companyLogo"></div>
-                        <div className="companyLogo"></div>
+                        {companyLogosImage}
                     </div>
                 </section>
             </main>
