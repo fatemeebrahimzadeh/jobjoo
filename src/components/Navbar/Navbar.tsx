0bf8967a608d69651051afce091c0c028cd9d59a
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-04-30 21:39:35 +0430

    complete Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import "./Navbar.scss"
 import logo from "../../assets/img/logo.png"
+import { Button, ButtonGroup } from "@mui/material";
 
 export default class Navbar extends Component {
     render() {
         return (
             <nav className="Navbar">
-                <div className="loginBtn"></div>
+                <ButtonGroup className="loginBtn">
+                    <Button
+                        size="medium"
+                        disabled
+                        onClick={() => { }}
+                        variant="text">ثبت نام</Button>
+                    <Button
+                        size="medium"
+                        disabled
+                        onClick={() => { }}
+                        variant="text">ورود</Button>
+                </ButtonGroup>
                 <div className="Navbar__logo">
                     <img src={logo} alt="" />
                 </div>
