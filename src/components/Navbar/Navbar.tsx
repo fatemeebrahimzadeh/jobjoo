0c72765a6d2eaabbf1fc802ceb63a2c7fd7816cb
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-04-30 22:45:01 +0430

    change style of button in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,65 @@
 import React, { Component } from "react";
 import "./Navbar.scss"
 import logo from "../../assets/img/logo.png"
-import { Button, ButtonGroup } from "@mui/material";
+import { Button, ButtonGroup, ThemeProvider } from "@mui/material";
+import { createTheme } from '@mui/material/styles';
+// typescript
+// declare module '@mui/material/styles' {
+//     interface Theme {
+//         status: {
+//             danger: React.CSSProperties['color'];
+//         };
+//     }
+
+//     interface Palette {
+//         neutral: Palette['primary'];
+//     }
+//     interface PaletteOptions {
+//         neutral: PaletteOptions['primary'];
+//     }
+
+//     interface PaletteColor {
+//         darker?: string;
+//     }
+//     interface SimplePaletteColorOptions {
+//         darker?: string;
+//     }
+//     interface ThemeOptions {
+//         status: {
+//             danger: React.CSSProperties['color'];
+//         };
+//     }
+// }
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            // Purple and green play nicely together.
+            main: "#fff",
+        }
+    },
+});
 
 export default class Navbar extends Component {
     render() {
         return (
             <nav className="Navbar">
                 <ButtonGroup className="loginBtn">
-                    <Button
-                        size="medium"
-                        disabled
-                        onClick={() => { }}
-                        variant="text">ثبت نام</Button>
-                    <Button
-                        size="medium"
-                        disabled
-                        onClick={() => { }}
-                        variant="text">ورود</Button>
+                    <ThemeProvider theme={theme}>
+                        <Button
+                            size="medium"
+                            color="primary"
+                            disabled
+                            onClick={() => { }}
+                            variant="text">ثبت نام</Button>
+                        <Button
+                            className="Button--left-H-line"
+                            size="medium"
+                            color="primary"
+                            disabled
+                            onClick={() => { }}
+                            variant="text">ورود</Button>
+                    </ThemeProvider>
                 </ButtonGroup>
                 <div className="Navbar__logo">
                     <img src={logo} alt="" />
