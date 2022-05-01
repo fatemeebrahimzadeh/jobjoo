e2e3a02111b8f2c3a97cff57edb5c407a2879019
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-05-01 18:07:02 +0430

    add RecruitmentBreif static

--- src/components/recruitmentBreif/RecruitmentBreif.tsx
+++ src/components/recruitmentBreif/RecruitmentBreif.tsx
@@ -2,18 +2,31 @@ import { render } from "@testing-library/react";
 import React, { Component } from "react";
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import CorporateFareIcon from '@mui/icons-material/CorporateFare';
+import PersonPinCircleIcon from '@mui/icons-material/PersonPinCircle';
 import "./RecruitmentBreif.scss"
+import { Button } from "@mui/material";
 
 export default class RecruitmentBreif extends Component {
     render() {
         return (
             <div className="RecruitmentBreif">
-                <BookmarkBorderIcon className="bookmark-icon" />
+
+                <div className="RecruitmentBreif--bookmark-icon">
+                    <BookmarkBorderIcon className="bookmark-icon" />
+                </div>
                 <h3 className="RecruitmentBreif--header">Senior UI/UX Designer</h3>
-                <h4>
-                    فناوران اطلاعات خبره | Fanavaran
-                    <CorporateFareIcon className="corporate-icon" />
-                </h4>
+                <div className="RecruitmentBreif--text">
+                    <h4>
+                        فناوران اطلاعات خبره | Fanavaran
+                        <CorporateFareIcon className="corporate-icon" />
+                    </h4>
+                    <h4>
+                        تهران
+                        <PersonPinCircleIcon className="location-icon" />
+                    </h4>
+                    <h6 className="time-span">سه روز پیش</h6>
+                </div>
+                <Button onClick={() => { }} variant="contained" size="large">مشاهده آگهی در جاباینجا</Button>
             </div>
         )
     }
