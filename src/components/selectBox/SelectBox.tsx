f97f247e89d1cd3a8029397da86e99aa524e7b58
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-05-01 14:09:06 +0430

    fix nug of selectBox

--- src/components/selectBox/SelectBox.tsx
+++ src/components/selectBox/SelectBox.tsx
@@ -12,51 +12,36 @@ export interface ISelectBoxOption {
 
 interface IProps {
     selectBoxElements: ISelectBoxOption[]
+    searchOnClickHandler(): void
 }
 
 export default class SelectBox extends Component<IProps> {
 
-    get selectBoxElements() {
-        return this.props.selectBoxElements.map(selectBoxElement => {
-            switch (selectBoxElement.type) {
-                case "Select":
-                    let options = selectBoxElement.options?.map(option => {
-                        return { label: option.name, id: option.id }
-                    })
-                    return ({ ...selectBoxElement, options })
-
-                case "TextField":
-                    return (selectBoxElement)
-            }
-        })
-    }
-
     render() {
 
-        // console.log("[selectBox]", this.props.selectBoxElements, this.selectBoxElements)
-
-        // let selectBoxElements = this.selectBoxElements.map((selectBoxElement, index) => {
-        //     switch (selectBoxElement.type) {
-        //         case "Select":
-        //             return <Autocomplete
-        //                 key={index}
-        //                 sx={{ width: `${selectBoxElement.width && selectBoxElement.width}` }}
-        //                 disablePortal
-        //                 id="combo-box-demo"
-        //                 options={selectBoxElement.options!}
-        //                 renderInput={(params) => <TextField {...params} label={selectBoxElement.label} />}
-        //             />
-        //         case "TextField":
-        //             return <TextField key={index} id="outlined-basic" label={selectBoxElement.label} variant="outlined" />
-        //     }
-        // })
+        let selectBoxElements = this.props.selectBoxElements.map((selectBoxElement, index) => {
+            switch (selectBoxElement.type) {
+                case "Select":
+                    return <Autocomplete
+                        key={index}
+                        sx={{ width: `${selectBoxElement.width && selectBoxElement.width}` }}
+                        disablePortal
+                        id="combo-box-demo"
+                        options={selectBoxElement.options!}
+                        getOptionLabel={(option) => option.name}
+                        renderInput={(params) => <TextField {...params} label={selectBoxElement.label} />}
+                    />
+                case "TextField":
+                    return <TextField key={index} id="outlined-basic" label={selectBoxElement.label} variant="outlined" />
+            }
+        })
 
         return (
             <div className="selectBox">
-                <IconButton className="selectBox__btn" onClick={() => { console.log("kug") }} aria-label="search">
+                <IconButton className="selectBox__btn" onClick={this.props.searchOnClickHandler} aria-label="search">
                     <SearchIcon />
                 </IconButton>
-                {/* {selectBoxElements} */}
+                {selectBoxElements}
             </div>
         )
     }
