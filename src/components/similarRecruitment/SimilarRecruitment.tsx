767ad6aba4caca125f494d3b71a75b07f6ba1aee
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-05-25 15:46:17 +0430

    SimilarRecruitment

--- src/components/similarRecruitment/SimilarRecruitment.tsx
+++ src/components/similarRecruitment/SimilarRecruitment.tsx
@@ -1,62 +1,27 @@
-import {Card} from "@mui/material";
+import { Card } from "@mui/material";
 import JobCard from "../jobCard/JobCard";
 import corporationLogo from "../../assets/img/corporation-logo.jpg";
+import { IRecruiment } from "../../@types/entities/recruiment";
 
 
 const cardStyle = {
     width: '100%',
     height: '180px',
     padding: '33px 20px 22px 39px',
-    marginTop :'15px',
+    marginTop: '15px',
     borderRadius: '15px',
     borderColor: 'rgba(112, 112, 112, 0.25)',
     backgroundColor: '#fff'
 }
 
-const SimilarRecruitment = () =>{
-    const recruitmentList=[
-        {
-            jobTitle : 'Senior UI/UX Designer',
-            requestDate :'سه روز پیش',
-            enCorporation : 'Fanavaran',
-            faCorporation : 'فناوران اطلاعات خبره',
-            city : 'تهران',
-            logoSrc : corporationLogo
-        },
-        {
-            jobTitle : 'Senior UI/UX Designer',
-            requestDate :'سه روز پیش',
-            enCorporation : 'Fanavaran',
-            faCorporation : 'فناوران اطلاعات خبره',
-            city : 'تهران',
-            logoSrc : corporationLogo
-        },
-        {
-            jobTitle : 'Senior UI/UX Designer',
-            requestDate :'سه روز پیش',
-            enCorporation : 'Fanavaran',
-            faCorporation : 'فناوران اطلاعات خبره',
-            city : 'تهران',
-            logoSrc : corporationLogo
-        },
-        {
-            jobTitle : 'Senior UI/UX Designer',
-            requestDate :'سه روز پیش',
-            enCorporation : 'Fanavaran',
-            faCorporation : 'فناوران اطلاعات خبره',
-            city : 'تهران',
-            logoSrc : corporationLogo
-        },
-        {
-            jobTitle : 'Senior UI/UX Designer',
-            requestDate :'سه روز پیش',
-            enCorporation : 'Fanavaran',
-            faCorporation : 'فناوران اطلاعات خبره',
-            city : 'تهران',
-            logoSrc : corporationLogo
-        },
-    ];
-    // return recruitmentList.map((recruitment,i) => <Card key={i} sx={cardStyle}><JobCard jobDetails={recruitment}/></Card>)
-    return <></>
+interface IProps {
+    recruitments: IRecruiment[]
+}
+
+const SimilarRecruitment = (props: IProps) => {
+
+    return (<>
+        {props.recruitments.map((recruitment, i) => <Card key={i} sx={cardStyle}><JobCard jobDetails={recruitment} /></Card>)}
+    </>)
 }
 export default SimilarRecruitment
--- src/pages/recruitment/Recruitment.tsx
+++ src/pages/recruitment/Recruitment.tsx
@@ -3,6 +3,9 @@ import RecruitmentSpecification from "../../components/recruitmentSpecification/
 import SimilarRecruitment from "../../components/similarRecruitment/SimilarRecruitment";
 import RecruitmentBrief from "../../components/recruitmentBrief/RecruitmentBrief";
 import { IRecruiment } from "../../@types/entities/recruiment";
+import { useEffect } from "react";
+import { Axios } from "../../utils/axios";
+import { AxiosResponse } from "axios";
 
 const titleStyle = {
     marginBottom: '15px',
@@ -20,7 +23,14 @@ const titleStyle = {
 
 const Recruitment = ({ jobDetails }: { jobDetails: IRecruiment }) => {
 
-    console.log("jobDetails", jobDetails)
+    const getData = async () => {
+        const response = await Axios.get(`/similar/ads/recruiment/${jobDetails.source.id}/`);
+        console.log(response)
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     return (
         <Grid container spacing={3} justifyContent='center'>
@@ -31,7 +41,7 @@ const Recruitment = ({ jobDetails }: { jobDetails: IRecruiment }) => {
                 <Stack>
                     <RecruitmentSpecification jobDetails={jobDetails} />
                     <Typography sx={titleStyle}>آگهی‌های مشابه</Typography>
-                    {/* <SimilarRecruitment /> */}
+                    <SimilarRecruitment recruitments={[]} />
                 </Stack>
             </Grid>
         </Grid>
